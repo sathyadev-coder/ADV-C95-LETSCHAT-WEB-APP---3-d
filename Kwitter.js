4ae565bc5b376428174f406a4fd6f990bf6289c7
sathyadev-coder <88343332+sathyadev-coder@users.noreply.github.com>
2022-09-11 11:45:00 +0530

    Update Kwitter.js

--- Kwitter.js
+++ Kwitter.js
@@ -1,10 +1,6 @@
 function addUser(){
 user_name =document.getElementById("user_name").value;
-console.log("user"+user_name);
-if(user_name != ""){ console.log("inside ff");
 localStorage.setItem("user_name" , user_name);
 window.location = "kwitter_room.html";
-
-}
 
 }
